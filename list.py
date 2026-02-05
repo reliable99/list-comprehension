d5043fe53e912d85a2a30e7b6daa5a651dd1b41f
reliable99 <abdulmuhminsodiq@gmail.com>
2026-02-05 07:16:27 +0100

    updating

--- list.py
+++ list.py
@@ -12,3 +12,48 @@ list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 flattened_list = [item for sublist in list_of_lists for item in sublist]
 
 print(flattened_list)
+
+
+
+result = [
+    (i, 1, i, i**2, i**3, i**4, i**5)
+    if i != 0 else (0, 1, 0, 0, 0, 0, 0)
+    for i in range(11)
+]
+
+print(result)
+
+
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+
+flattened = [
+    [country.upper(), country[:3].upper(), capital.upper()]
+    for [(country, capital)] in countries
+]
+
+print(flattened)
+
+
+countries = [[('Finland', 'Helsinki')], [('Sweden', 'Stockholm')], [('Norway', 'Oslo')]]
+
+result = [
+    {'country': country.upper(), 'city': city.upper()}
+    for [(country, city)] in countries
+]
+
+print(result)
+
+
+names = [
+    [('Asabeneh', 'Yetayeh')],
+    [('David', 'Smith')],
+    [('Donald', 'Trump')],
+    [('Bill', 'Gates')]
+]
+
+result = [
+    f"{first} {last}"
+    for [(first, last)] in names
+]
+
+print(result)
